db5093a1b70905f8d3b695b4e51f7fdc817aa6a5
Petr Sidorov <piterpatrikk@gmail.com>
2023-07-28 20:26:57 +0400

    more react

--- react/react-v8-course/src/App.jsx
+++ react/react-v8-course/src/App.jsx
@@ -1,18 +1,30 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParam";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Queryclient, QueryclientProvider } from '@tanstack/react-query';
 import Details from "./Details";
+
+const queryClient = new Queryclient({
+    defaltOptions: {
+        queries: {
+            stateTime: Infinity,
+            cacheTime: Infinity,
+        }
+    }
+})
 
 const App = () => {
     return (
         <BrowserRouter>
-            <header>
-            <Link to="/">Adopt me!</Link>
-            </header>
-            <Routes>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-            </Routes>
+            <QueryclientProvider client={queryClient}>
+                <header>
+                <Link to="/">Adopt me!</Link>
+                </header>
+                <Routes>
+                    <Route path="/details/:id" element={<Details />} />
+                    <Route path="/" element={<SearchParams />} />
+                </Routes>
+            </QueryclientProvider>
         </BrowserRouter>
     )
 }
--- react/react-v8-course/src/Details.jsx
+++ react/react-v8-course/src/Details.jsx
@@ -1,8 +1,28 @@
 import { useParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
     const { id } = useParams();
-    return <h2>{id}</h2>;
+    const results = useQuery(["details", id], fetchPet);
+
+    if (results.isLoading) {
+        return (
+            <div className="loading-pane">
+                <h2 className="loader">@</h2>
+            </div>
+        );
+    }
+
+    const pet = results.data.pets[0];
+    <div className="className de"></div>
+    return (
+        <div className="details">
+            <h1>{pet.pet}</h1>
+            <h2>{pet.animal}</h2>
+        </div>
+
+    );
 };
 
 export default Details;
