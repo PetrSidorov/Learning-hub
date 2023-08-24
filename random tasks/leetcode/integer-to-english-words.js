e72f39fef807fe4fae6fee5834fb370931bbdede
Petr Sidorov <piterpatrikk@gmail.com>
2023-08-24 15:46:06 +0400

    leetcode problems

---
+++ random tasks/leetcode/integer-to-english-words.js
@@ -0,0 +1,61 @@
+// Convert a non-negative integer num to its English words representation.
+// Example 1:
+
+// Input: num = 123
+// Output: "One Hundred Twenty Three"
+// Example 2:
+
+// Input: num = 12345
+// Output: "Twelve Thousand Three Hundred Forty Five"
+// Example 3:
+
+// Input: num = 1234567
+// Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var numberToWords = function (num) {
+    const numberWords = {
+        0: 'Zero'
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+        10: 'Ten',
+        11: 'Eleven',
+        12: 'Twelve',
+        13: 'Thirteen',
+        14: 'Fourteen',
+        15: 'Fifteen',
+        16: 'Sixteen',
+        17: 'Seventeen',
+        18: 'Eighteen',
+        19: 'Nineteen',
+        20: 'Twenty',
+        30: 'Thirty',
+        40: 'Forty',
+        50: 'Fifty',
+        60: 'Sixty',
+        70: 'Seventy',
+        80: 'Eighty',
+        90: 'Ninety',
+    };
+    
+    const placeWords = {
+        100: 'Hundred',
+        1000: 'Thousand',
+        1000000: 'Million',
+        1000000000: 'Billion',
+    };
+    
+    
+};
+
+console.log(numberToWords);
--- random tasks/leetcode/integer-to-roman.js
+++ random tasks/leetcode/integer-to-roman.js
@@ -14,42 +14,124 @@
 // Output: "MCMXCIV"
 // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
+// Input: num = 4
+// Output: "IV"
+// Explanation: IV represents 4.
+
+// Input: num = 9
+// Output: "IX"
+// Explanation: IX represents 9.
+
+// Input: num = 14
+// Output: "XIV"
+// Explanation: X = 10 and IV = 4.
+
+// Input: num = 44
+// Output: "XLIV"
+// Explanation: XL = 40 and IV = 4.
+
+// Input: num = 89
+// Output: "LXXXIX"
+// Explanation: L = 50, XXX = 30, and IX = 9.
+
+// Input: num = 144
+// Output: "CXLIV"
+// Explanation: C = 100, XL = 40 and IV = 4.
+
+// Input: num = 399
+// Output: "CCCXCIX"
+// Explanation: CCC = 300, XC = 90, and IX = 9.
+
+// Input: num = 487
+// Output: "CDLXXXVII"
+// Explanation: CD = 400, LXXX = 80, and VII = 7.
+
+// Input: num = 888
+// Output: "DCCCLXXXVIII"
+// Explanation: DCCC = 800, LXXX = 80, and VIII = 8.
+
+// Input: num = 2019
+// Output: "MMXIX"
+// Explanation: MM = 2000, X = 10, and IX = 9.
+
 /**
  * @param {number} num
  * @return {string}
  */
 var intToRoman = function (num) {
-  const romanInt = {
-    9: "IX",
-    4: "IV",
-    40: "XL",
-    90: "XC",
-    400: "CD",
-    900: "CM",
-    1: "I",
-    5: "V",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    1000: "M",
+  romanThousand = {
+    int: {
+      1000: "M",
+    },
+    intSub: {},
+  };
+  romanHundreds = {
+    intSub: {
+      400: "CD",
+      900: "CM",
+    },
+    int: {
+      500: "D",
+      100: "C",
+    },
   };
 
-  function countSum(num) {
-    for (const key in romanInt) {
-        if romanInt.hasOwnProperty(key) {
-            const roman = romanInt[key];
-            
-        }
+  romanTens = {
+    intSub: {
+      90: "XC",
+      40: "XL",
+    },
+    int: {
+      50: "L",
+      10: "X",
+    },
+  };
+
+  romanUnits = {
+    intSub: {
+      9: "IX",
+      4: "IV",
+    },
+    int: {
+      5: "V",
+      1: "I",
+    },
+  };
+
+  const decode = [romanUnits, romanTens, romanHundreds, romanThousand];
+  function getSummedRoman(partOfNumber, i, toReturn) {
+    const arrayToWorkWith = Object.entries(decode[i].int).reverse();
+    for (let key of arrayToWorkWith) {
+      if (partOfNumber >= key[0]) {
+        partOfNumber = partOfNumber - key[0];
+        toReturn.push(key[1]);
+        getSummedRoman(partOfNumber, i, toReturn);
+        break;
+      }
     }
+    return toReturn;
+  }
+  function getRoman(partOfNumber, i) {
+    return (
+      decode[partOfNumber.toString().length - 1].int[partOfNumber] ||
+      decode[partOfNumber.toString().length - 1].intSub[partOfNumber] ||
+      getSummedRoman(partOfNumber, partOfNumber.toString().length - 1, [])
+    );
   }
 
   num = num.toString().split("");
   for (let i = 0; i < num.length; i++) {
     num[i] = num[i] * Math.pow(10, num.length - i - 1);
-    num[i] = romanInt[num[i]] ? romanInt[num[i]] : countSum(num[i]);
+    num[i] = getRoman(num[i], i);
   }
-  return num;
+  return num.join("").replace(/,/g, "");
 };
 
-console.log(intToRoman(58));
+console.log(intToRoman(108));
+// Input: num = 888
+// Output: "DCCCLXXXVIII"
+// Explanation: DCCC = 800, LXXX = 80, and VIII = 8.
+
+// Input: num = 1994
+// Output: "MCMXCIV"
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
