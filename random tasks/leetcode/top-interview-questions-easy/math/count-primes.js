e5088d97d3fcd39665b08a9d7ed0efc774e6cffe
Petr Sidorov <piterpatrikk@gmail.com>
2023-10-28 15:07:50 +0400

    count primes attempt, min stack in progress

--- random tasks/leetcode/top-interview-questions-easy/math/count-primes.js
+++ random tasks/leetcode/top-interview-questions-easy/math/count-primes.js
@@ -52,22 +52,22 @@
  * @return {number}
  */
 var countPrimes = function (n) {
-  function removePow(number) {
-    let k = 2;
-    let indexOfPow = 1;
-    while (indexOfPow > -1) {
-      powNumberToDelete = number * k;
-      console.log("number Pow: ", number);
-      console.log("k: ", k);
-      console.log("powNumberToDelete: ", powNumberToDelete);
-      indexOfPow = allNumbers.indexOf(powNumberToDelete);
-      if (indexOfPow > -1) {
-        allNumbers.splice(indexOfPow, 1);
-        console.log("sucess~");
-      }
-      k++;
-    }
-  }
+  // function removeMultiple(number) {
+  //   let k = 2;
+  //   let indexOfPow = 1;
+  //   while (indexOfPow > -1) {
+  //     powNumberToDelete = number * k;
+  //     console.log("number Pow: ", number);
+  //     console.log("k: ", k);
+  //     console.log("powNumberToDelete: ", powNumberToDelete);
+  //     indexOfPow = allNumbers.indexOf(powNumberToDelete);
+  //     if (indexOfPow > -1) {
+  //       allNumbers.splice(indexOfPow, 1);
+  //       console.log("sucess~");
+  //     }
+  //     k++;
+  //   }
+  // }
   let allNumbers = Array.from({ length: n - 2 }, (v, i) => i + 2);
   console.log("allNumbers: ", allNumbers);
   for (let i = 2; i <= n; i++) {
@@ -78,20 +78,187 @@ var countPrimes = function (n) {
         // allNumbers.splice(i, 1);
         let indexOfNum = allNumbers.indexOf(i);
         let number = allNumbers[indexOfNum];
-        removePow(number);
+        // removeMultiple(number);
         if (indexOfNum > -1) {
           allNumbers.splice(indexOfNum, 1);
         }
+      } else {
+        console.log("i is prime: ", i);
       }
     }
     console.log("------");
   }
-  return allNumbers.length;
+  return allNumbers;
 };
 
-console.log(countPrimes(499979));
+// console.log(countPrimes(10));
 
 // 6n + 1 or 6n – 1
-// var countPrimes2 = function (n) {
-//   for (let i = 0; i < n; i++) {}
-// };
+var countPrimes2 = function (n) {
+  let counter = 0;
+  let allNumbers = Array.from({ length: n - 2 }, (v, i) => i + 2);
+  console.log("allNumbers: ", allNumbers);
+  sqrtLoop: for (let i = 2; i <= n; i++) {
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      console.log("i, j: ", i, j);
+      if (i % j == 0) {
+        console.log("i % j:", i % j);
+        let indexOfNum = allNumbers.indexOf(i);
+        let number = allNumbers[indexOfNum];
+        if (indexOfNum > -1) {
+          console.log("number to splice: ", number);
+          allNumbers.splice(indexOfNum, 1);
+          counter++;
+          // break sqrtLoop;
+        }
+      } else {
+        console.log("i is prime: ", i);
+      }
+    }
+    console.log("------");
+  }
+  // console.log("counter: ", counter);
+  console.log("counter?: ", n - counter - 2);
+  console.log("allNumbers.length: ", allNumbers.length);
+  return allNumbers;
+};
+
+// console.log(countPrimes2(2350));
+
+// -----
+var countPrimes3 = function (n) {
+  let counter = 0;
+  // let allNumbers = Array.from({ length: n - 2 }, (v, i) => i + 2);
+  // console.log("allNumbers: ", allNumbers);
+  for (let i = 2; i <= n; i++) {
+    sqrtLoop: for (let j = 2; j <= Math.sqrt(i); j++) {
+      console.log("i, j: ", i, j);
+      if (i % j == 0) {
+        console.log("i % j:", i % j);
+        // let indexOfNum = allNumbers.indexOf(i);
+        // let number = allNumbers[indexOfNum];
+        // if (indexOfNum > -1) {
+        // console.log("number to splice: ", number);
+        // allNumbers.splice(indexOfNum, 1);
+        counter++;
+        break sqrtLoop;
+        // }
+      } else {
+        console.log("i is prime: ", i);
+      }
+    }
+    console.log("------");
+  }
+  // console.log("counter: ", counter);
+  console.log("counter?: ", n - counter - 1);
+  // console.log("allNumbers.length: ", allNumbers.length);
+  // return allNumbers;
+};
+
+// console.log(countPrimes3(10));
+
+//-------
+
+var countPrimes4 = function (n) {
+  let counter = 0;
+  for (let i = 1; i < n; i++) {
+    sqrtLoop: for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j == 0) {
+        counter++;
+        break sqrtLoop;
+      }
+    }
+  }
+
+  return n - counter - 2 < 0 ? 0 : n - counter - 2;
+};
+
+// console.log("primes: ", countPrimes4(461465));
+// console.log("primes: ", countPrimes4(433933));
+
+// console.log(countPrimes4(15));
+// 2, 3, 5, 7, 11, and 13.
+12357 - 1;
+2357;
+
+var countPrimes5 = function (n) {
+  let counter = 0;
+  let realCounter = 0;
+  let primes = [];
+  if (n == 3) {
+    primes.push(2);
+  }
+  if (n > 3) {
+    primes.push(2, 3);
+  }
+  for (let i = 1; i < n; i++) {
+    sqrtLoop: for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j == 0) {
+        counter++;
+        break sqrtLoop;
+      } else {
+        realCounter++;
+        console.log("i: ", i);
+        primes.push(i);
+        j = i;
+        break;
+      }
+    }
+  }
+
+  if (n == 3) {
+    realCounter++;
+  }
+
+  if (n > 3) {
+    realCounter += 2;
+  }
+
+  console.log("primes: ", primes);
+
+  console.log("real counter: ", realCounter);
+  return n - counter - 2 < 0 ? 0 : n - counter - 2;
+};
+
+// console.log(countPrimes4(2));
+// console.log(countPrimes5(10));
+
+var countPrimes6 = function (n) {
+  let counter = 0;
+  let primes = [];
+  if (n == 3) {
+    primes.push(2);
+  }
+  if (n > 3) {
+    primes.push(2, 3);
+  }
+  x = 3;
+  for (let i = 1; i < n; i++) {
+    sqrtLoop: for (let j = 0; j < x; j++) {
+      if (i % j == 0) {
+        break sqrtLoop;
+      } else {
+        counter++;
+        console.log("i: ", i);
+        primes.push(i);
+        j = i;
+        break;
+      }
+    }
+    // sqrtLoop: for (let j = 2; j <= Math.sqrt(i); j++) {
+    //   if (i % j == 0) {
+    //     break sqrtLoop;
+    //   } else {
+    //     counter++;
+    //     console.log("i: ", i);
+    //     primes.push(i);
+    //     j = i;
+    //     break;
+    //   }
+    // }
+  }
+  console.log("primes: ", primes);
+};
+
+// console.log(countPrimes4(2));
+console.log(countPrimes6(10));
